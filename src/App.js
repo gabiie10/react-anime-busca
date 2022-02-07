b601597926beca528a84181aa5b8983e0232ac4c
Gabriela dos Santos Lamar <97289224+gabiie10@users.noreply.github.com>
2022-02-07 14:46:26 -0300

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import SearchInput from './SearchInput';
 
 function App() {
+  const[info,setInfo]=useState({});//serve pra pegar os elementos do api e atualiza-los
+  const [text,setText]=useState('');//so pega e autalizar o que vai ser escrito na barra de pequisa
+  useEffect(() => {
+    if(text){
+      setInfo({});
+      fetch(`https://kitsu.io/api/edge/anime?filter[text]=${text}&page[limit]=12`)
+          .then((response)=>response.json())
+          .then((response)=>{
+            setInfo(response);//atualiza o info,agora info recebera o valor da response       
+          });
+    }
+  },[text]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animê</h1>
+      <SearchInput 
+        value={text} 
+        onChange={(search)=>setText(search)}
+      />
+      {text && !info.data &&(//info.data agora sera a mesma coisa de response.data  
+        <span>Carregando...</span>
+      )}
+        {info.data && (
+          <ul className="animes-list">
+            {info.data.map((anime)=>(// O map serve pra listar todos os elementos da api pegando o array onde os daados estao(no caso info.data).Sempre q eu faço um map eu tenho que pro uma key
+              <li key={anime.id}> 
+                  <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
+                  {anime.attributes.canonicalTitle}
+              </li>
+          ))}
+          </ul>
+        )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/SearchInput.js
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+import useDebounce from "./useDebounce";
+const SearchInput=({value,onChange})=>{
+    const[displayValue,setDisplayValue]=useState(value);
+    const debouncedChange=useDebounce(onChange,500);
+    function handleChange(event){
+        setDisplayValue(event.target.value);
+        debouncedChange(event.target.value);
+    }
+return(
+    <input type="search" value={displayValue} onChange={handleChange}/>//a barra de pesquisa
+);
+}
+export default SearchInput;
